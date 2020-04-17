13408c2d0edfd251a893c2790b0424dda72ff344
Jessica <Jessica@users-iMac.local>
2020-04-16 21:12:28 -0400

    seeds & migrations

---
+++ data/migrations/20200416184039_initial-tables.js.js
@@ -0,0 +1,67 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  
+  // Manage recipes.
+    .createTable('recipes', tbl => {
+      tbl.increments(); 
+      tbl.string('recipe_title', 128)
+         .unique()
+         .notNullable()
+      tbl.text('description')
+      tbl.float('servings'); 
+  
+    })
+
+  // Manage ingredients.  
+    .createTable('ingredients', tbl => {
+      tbl.increments();
+      tbl.string('ingredient_name', 128)
+         .unique()
+         .notNullable()
+      tbl.float('quantity')
+         .notNullable()
+      tbl.string('measurement', 128)
+         .notNullable();
+    })
+
+  // Manage recipes ingredients.
+    .createTable('recipe_ingredients', tbl => {
+      tbl.increments();
+      tbl.integer('recipe_id') // reference to recipes
+         .unsigned()
+         .notNullable()
+         .references('recipes.id')
+         .onUpdate('CASCADE')
+         .onDelete('CASCADE')
+      tbl.integer('ingredient_id') // reference to ingredients
+         .unsigned()
+         .notNullable()
+         .references('ingredients.id')
+         .onUpdate('CASCADE')
+         .onDelete('CASCADE');
+    })
+
+  // Manage recipes steps. 
+    .createTable('recipe_steps', tbl => {
+      tbl.increments();
+      tbl.integer('recipe_id') // reference to recipes
+         .unsigned()
+         .notNullable()
+         .references('recipes.id')
+         .onUpdate('CASCADE')
+         .onDelete('CASCADE')
+      tbl.integer('step_number')
+         .notNullable()
+      tbl.text('description')
+         .notNullable();
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema  
+    .dropTableIfExists('recipe_steps')
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
+};
